fa7322f9a384110f0e4f606b4affd7f7fd0ee11e
Stefan Ene <enes@oregonstate.edu>
2025-12-05 16:19:50 -0800

    working on 32bit mode

---
+++ old_boot/boot_sect.asm
@@ -0,0 +1,32 @@
+[org 0x7c00]
+
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, 0x9000
+mov dh, 2
+
+call disk_load
+
+mov dx, [0x9000]
+call print_hex
+call print_nl
+
+mov dx, [0x9000 + 512]
+call print_hex
+
+
+jmp $
+
+%include "boot_print.asm"
+%include "boot_print_hex.asm"
+%include "boot_disk.asm"
+
+
+; fill with 510 zeros minus prev code 
+times 510-($-$$) db 0
+; magic number!
+dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
